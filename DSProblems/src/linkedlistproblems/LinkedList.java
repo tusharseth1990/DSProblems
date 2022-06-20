805c725dab7812b9e6d47c1fae670df83d81d1c8
TUSHAR SETH <tusharseth1990@gmail.com>
2022-06-20 07:34:50 +0530

    Update LinkedList.java

--- DSProblems/src/linkedlistproblems/LinkedList.java
+++ DSProblems/src/linkedlistproblems/LinkedList.java
@@ -165,7 +165,7 @@ public class LinkedList {
 	 
 //	 METHOD 2 (By reversing the list)
 //	 This method takes O(n) time and O(1) extra space.
-//	 1) Get the middle of the linked list.
+//	 1) Get the middle of the linked list i.e. end of first hf 
 //	 2) Reverse the second half of the linked list.
 //	 3) Check if the first half and second half are identical.
 //	 4) Construct the original linked list by reversing the second half again and attaching it back to the first half
