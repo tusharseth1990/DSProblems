ac00656f11d1faa36ef1c0e67dfa8da9294be556
TUSHAR SETH <tusharseth1990@gmail.com>
2022-06-20 07:32:42 +0530

    Update LinkedList.java

--- DSProblems/src/linkedlistproblems/LinkedList.java
+++ DSProblems/src/linkedlistproblems/LinkedList.java
@@ -170,7 +170,54 @@ public class LinkedList {
 //	 3) Check if the first half and second half are identical.
 //	 4) Construct the original linked list by reversing the second half again and attaching it back to the first half
 //	 
-	 
+	
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }        
+
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    // Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+ 
+	
 	 /* Function to print middle of linked list */
 	    void printMiddle() 
 	    { 
