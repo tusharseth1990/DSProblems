5f7c59da7811862bca2d2340a5020b2a1657b787
seth <tusharseth1990@gmail.com>
2022-08-31 23:03:58 +0530

    Fizzbuzz

---
+++ src/array/FizzBuzz.java
@@ -0,0 +1,68 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+
+        // ans list
+        List<String> ans = new ArrayList<String>();
+
+        for (int num = 1; num <= n; num++) {
+
+            boolean divisibleBy3 = (num % 3 == 0);
+            boolean divisibleBy5 = (num % 5 == 0);
+
+            if (divisibleBy3 && divisibleBy5) {
+                // Divides by both 3 and 5, add FizzBuzz
+                ans.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                // Divides by 3, add Fizz
+                ans.add("Fizz");
+            } else if (divisibleBy5) {
+                // Divides by 5, add Buzz
+                ans.add("Buzz");
+            } else {
+                // Not divisible by 3 or 5, add the number
+                ans.add(Integer.toString(num));
+            }
+        }
+
+        return ans;
+    }
+
+
+    public List<String> fizzBuzzMethod2(int n) {
+        // ans list
+        List<String> ans = new ArrayList<String>();
+
+        for (int num = 1; num <= n; num++) {
+
+            boolean divisibleBy3 = (num % 3 == 0);
+            boolean divisibleBy5 = (num % 5 == 0);
+
+            String numAnsStr = "";
+
+            if (divisibleBy3) {
+                // Divides by 3, add Fizz
+                numAnsStr += "Fizz";
+            }
+
+            if (divisibleBy5) {
+                // Divides by 5, add Buzz
+                numAnsStr += "Buzz";
+            }
+
+            if (numAnsStr.equals("")) {
+                // Not divisible by 3 or 5, add the number
+                numAnsStr += Integer.toString(num);
+            }
+
+            // Append the current answer str to the ans list
+            ans.add(numAnsStr);
+        }
+
+        return ans;
+    }
+}
