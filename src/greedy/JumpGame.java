1f584bc792a132640c8473bf1174142ab4d3eea8
seth <tusharseth1990@gmail.com>
2022-10-07 00:10:48 +0530

    code changes greedy

---
+++ src/greedy/JumpGame.java
@@ -0,0 +1,14 @@
+package greedy;
+
+public class JumpGame {
+
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] + i >= goal) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+}
