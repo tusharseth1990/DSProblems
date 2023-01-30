6237738737653cc49eafdfa66d63f186d7f9a0bb
seth <tusharseth1990@gmail.com>
2023-01-30 15:31:51 +0530

    Bitwise programs

---
+++ src/bitwise/SingleNumber.java
@@ -0,0 +1,12 @@
+package bitwise;
+//We can use xor operation as it cancel out itself (i.e. only when values are different
+// in binary representation then give output). See how xor operation works if confused.
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) ans ^= nums[i];
+        return ans;
+    }
+}
