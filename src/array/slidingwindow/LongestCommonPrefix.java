3b59181e30668dc2d7cdbb12887849e4aa2593ef
TUSHAR SETH <tusharseth1990@gmail.com>
2023-07-07 17:40:04 +0530

    Update LongestCommonPrefix.java

--- src/array/slidingwindow/LongestCommonPrefix.java
+++ src/array/slidingwindow/LongestCommonPrefix.java
@@ -1,7 +1,20 @@
 package array.slidingwindow;
 
 public class LongestCommonPrefix {
+    
+/*
+https://leetcode.com/problems/longest-common-prefix/
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+Example 1:
 
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+*/    
 
     static String commonPrefix(String[] arr)
     {
