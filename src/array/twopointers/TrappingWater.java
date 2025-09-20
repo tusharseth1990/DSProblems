b9ed9e7deebe710e84b9fff31881c37938598732
seth <tusharseth1990@gmail.com>
2025-09-20 11:04:40 +0530

    modified changes

--- src/array/twopointers/TrappingWater.java
+++ src/array/twopointers/TrappingWater.java
@@ -3,6 +3,36 @@ package array.twopointers;
 //sol2: L , R pointer using maxL , maxR shift smaller one Min(MaxL,MaxR)
 
 public class TrappingWater {
+//sol1: min(L,R) - height[i]
+    public int trapSol1(int[] height) {
+        int[] left = maxToLeft(height);
+        int[] right = maxToRight(height);
+        int totalWater = 0;
+        for (int i = 0; i < height.length; i++) {
+            totalWater += Math.min(left[i], right[i]) - height[i];
+        }
+        return totalWater;
+    }
+
+    public int[] maxToLeft(int[] height) {
+        int[] left = new int[height.length];
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            max = Math.max(max, height[i]);
+            left[i] = max;
+        }
+        return left;
+    }
+
+    public int[] maxToRight(int[] height) {
+        int[] right = new int[height.length];
+        int max = 0;
+        for (int i = height.length - 1; i >= 0; i--) {
+            max = Math.max(max, height[i]);
+            right[i] = max;
+        }
+        return right;
+    }
 
     public int trap(int[] heights) {
         if (heights.length == 0) return 0;
